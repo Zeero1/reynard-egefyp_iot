495d9df0eb33cc3424872833f293450e8fffb7ea
Zeero1 <reynardyee@gmail.com>
2024-02-01 11:39:38 +0800

    Update consumers.py

--- egefyp_iot/devicewebservice/devicewebapp/consumers.py
+++ egefyp_iot/devicewebservice/devicewebapp/consumers.py
@@ -16,12 +16,6 @@ class GraphConsumer(AsyncWebsocketConsumer):
             output_signal_cmd = subprocess.run(["iw", "dev", "wlan1", "station", "dump"], capture_output=True, text=True, check=True)
             output_signal = output_signal_cmd.stdout
 
-            # Sample Output from "iw dev wlan1 station dump"
-            # output_signal = """Station 3c:9c:0f:61:3b:1d (on wlan1)
-            # signal:         -37 dBm
-            # Station 9c:9c:0d:11:3b:1d (on wlan1)
-            # signal:         -40 dBm"""
-
             # Split the lines into a list
             signal_lines = output_signal.splitlines()
 
@@ -43,7 +37,7 @@ class GraphConsumer(AsyncWebsocketConsumer):
                     signal = nospc_lines_signal[i].replace("signal:", '')
                     signal_cut = int(signal.replace("dBm", ''))
                     signal_list.append((mac_address , signal , signal_cut))
-                    
+            
 
             print(signal_list)
             # [('3c:9c:0f:61:3b:1d', '-37dBm'), ('9c:9c:0d:11:3b:1d', '-40dBm')]
