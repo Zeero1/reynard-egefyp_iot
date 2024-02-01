9d20819478618f432f5a920aa06f8efd869ee84d
Zeero1 <reynardyee@gmail.com>
2024-02-01 11:50:41 +0800

    Update consumers.py

--- egefyp_iot/devicewebservice/devicewebapp/consumers.py
+++ egefyp_iot/devicewebservice/devicewebapp/consumers.py
@@ -69,7 +69,7 @@ class GraphConsumer(AsyncWebsocketConsumer):
                         matches = pattern.findall(arp_line)
                         connected_devices.extend(matches)
                         print(connected_devices)
-            await self.send(json.dumps(signal_list))
+            await self.send(json.dumps(signal_list, connected_devices))
             del signal_list [ : ]
             del connected_devices [ : ]
             await sleep(1)
