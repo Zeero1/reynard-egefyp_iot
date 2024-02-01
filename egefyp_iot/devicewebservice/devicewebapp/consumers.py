c315df8141237ca52f4da42a0eec4a06c670e3f4
Zeero1 <reynardyee@gmail.com>
2024-02-01 11:42:19 +0800

    Update consumers.py

--- egefyp_iot/devicewebservice/devicewebapp/consumers.py
+++ egefyp_iot/devicewebservice/devicewebapp/consumers.py
@@ -12,6 +12,8 @@ class GraphConsumer(AsyncWebsocketConsumer):
         await self.accept()
         
         for i in range(1000):
+            signal_list.clear()
+            connected_devices.clear()
             # Command to get signal strength
             output_signal_cmd = subprocess.run(["iw", "dev", "wlan1", "station", "dump"], capture_output=True, text=True, check=True)
             output_signal = output_signal_cmd.stdout
