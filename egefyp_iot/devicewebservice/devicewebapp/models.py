948fc4508ac759b39e4e3b3d45529540e506ae45
Zeero1 <reynardyee@gmail.com>
2024-02-08 11:08:25 +0800

    test

--- egefyp_iot/devicewebservice/devicewebapp/models.py
+++ egefyp_iot/devicewebservice/devicewebapp/models.py
@@ -31,3 +31,6 @@ class Device(models.Model):
     macaddr = models.CharField(max_length=30)
     signalstr = models.CharField(max_length=10)
 
+    def __str__(self):
+        return f"{self.hostnm} {self.ipaddr} {self.macaddr} {s}"
+
