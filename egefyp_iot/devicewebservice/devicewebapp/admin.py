d761c3dc1204eb444cc292442c2b84cbfe58884d
Zeero1 <reynardyee@gmail.com>
2024-02-09 12:59:58 +0800

    Update admin.py

--- egefyp_iot/devicewebservice/devicewebapp/admin.py
+++ egefyp_iot/devicewebservice/devicewebapp/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from devicewebapp.models import UserProfileInfo, Device
 
 # Register your models here.
-admin.site.register(UserProfileInfo, Device)
+admin.site.register(UserProfileInfo)
+admin.site.register(Device)
