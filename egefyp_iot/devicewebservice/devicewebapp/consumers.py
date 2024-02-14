5d44710085bd4ba055caa65ad71ff4c743c10890
Zeero1 <reynardyee@gmail.com>
2024-02-15 00:26:28 +0800

    Sending devices code using views.py

--- egefyp_iot/devicewebservice/devicewebapp/consumers.py
+++ egefyp_iot/devicewebservice/devicewebapp/consumers.py
@@ -1,44 +1,18 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+import asyncio
+import traceback
+from channels.generic.websocket import AsyncWebsocketConsumer  
+from .views import *
 
 class GraphConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_group_name = 'device'
-
-        # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-
         await self.accept()
 
-    async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+    async def send_message(self, message_data):
+        await self.send(json.dumps(message_data))
 
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+    async def send_error(self, error_message):
+        await self.send(text_data=json.dumps({'error': error_message}))
 
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'kafka_message',
-                'message': message
-            }
-        )
-
-    # Receive message from room group
-    async def kafka_message(self, event):
-        message = event['message']
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+    async def disconnect(self):
+        pass
